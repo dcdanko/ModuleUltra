d34092558a2ddead202b271ce45adab7a9d30222
Danko David <dcd3001@hippocampus.pbtech>
2017-11-19 23:21:08 -0500

    added snakemake utils

---
+++ moduleultra/snakemake_utils.py
@@ -0,0 +1,38 @@
+from .utils import joinResultNameType
+import datasuper as ds
+
+
+def getOriginResultFiles(config, resultType, fileType):
+    '''
+    N.B. This function returns another function!
+    It does not return the filepath itself
+    '''
+
+    def getter(wcs):
+        try:
+            return config['origins'][resultType][wcs.sample_name][fileType]
+        except AttributeError:
+            return config['origins'][resultType][wcs.group_name][fileType]
+
+    return getter
+
+
+def expandGroup(*samplePatterns):
+    '''
+    N.B. This function returns another function!
+    It does not return the filepaths themselves
+    '''
+    def getter(wcs):
+        gname = wcs.group_name
+        dsrepo = ds.Repo.loadRepo()
+        group = dsrepo.db.sampleGroupTable.get(gname)
+
+        patterns = []
+        for sample in group.allSamples():
+            for pattern in samplePatterns:
+                p = pattern.format(sample_name= sample.name)
+                patterns.append(p)
+        return patterns
+
+    return getter
+
